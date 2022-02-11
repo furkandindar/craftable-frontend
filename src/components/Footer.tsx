4e3ff1307240422400fa69d206dcc2310c5c7725
furkandindar <fdindar1@binghamton.edu>
2022-02-11 23:07:34 +0300

    implement fully responsive footer and footer styling improvements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ import CustomButton from './CustomButton';
 
 const siteLinks = ['Explore','Company','Bond','Craft','Governance','Connect Wallet','Marketplace'];
 const otherLinks = ['My Dashboard',"NFT's","New"];
-const helpLinks = ['Help Desk', 'Who We Are', 'Who We Are', 'How To Craft', 'How To Create Chest', 'How To Create NFT'];
+const helpLinks = ['Help Desk', 'Who We Are', 'How To Craft', 'How To Create Chest', 'How To Create NFT'];
 
 const WhiteTypography = styled(Typography)(({ theme }) => ({
     ...theme.typography,
@@ -95,10 +95,9 @@ const Footer = () => {
                   <Grid item container>
                     <Grid item xs={12} md={6}>
                         <Stack direction="column" spacing={1}>
-                        <WhiteTypography fontWeight="bold">Keep in touch</WhiteTypography>
-                        <WhiteTypography paddingBottom={1} variant="body2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</WhiteTypography>
+                        <WhiteTypography sx={{textAlign:{xs:"center", md:"left"}}} fontWeight="bold">Keep in touch</WhiteTypography>
+                        <WhiteTypography sx={{textAlign:{xs:"center", md:"left"}}} paddingBottom={1} variant="body2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</WhiteTypography>
                         <Grid container direction="row" spacing={1}>
-                            {/* <CustomInput placeholder='Your email address' aria-label="email address"/> */}
                             <Grid item xs={9} sx={{'&.MuiGrid-item':{paddingLeft:0}}}>
                             <TextField sx={{ input: { backgroundColor:"white", borderRadius:1 } }} placeholder="Your email address" fullWidth size='small'></TextField>
                             </Grid>
@@ -108,7 +107,7 @@ const Footer = () => {
                         </Grid>
                         </Stack>
                     </Grid>
-                    <Grid item container xs={12} md={6} alignItems="flex-end" justifyContent="flex-end">
+                    <Grid item container xs={12} md={6} alignItems="flex-end" sx={{justifyContent:{xs:"center", md:"flex-end"},padding:{xs:2,md:0}}}>
                         <Stack direction="row" spacing={3}>
                             <FacebookIcon fontSize="large"/>
                             <InstagramIcon fontSize="large"/>
@@ -122,28 +121,28 @@ const Footer = () => {
                   <Grid item xs={12} container direction="row" spacing={5} justifyContent="space-between">
                       <Grid item xs={12} md={4}>
                           <Stack direction="column" spacing={5}>
-                                  <Box>
+                                  <Box sx={{display:"flex", justifyContent:{xs:"center", md:"flex-start"}}}>
                                   <img src={require("../assets/footer_logo.png")}/>
                                   </Box>
-                                  <Typography >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</Typography>
+                                  <Typography sx={{textAlign:{xs:"center", md:"left"}}} >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</Typography>
                           </Stack>
                       </Grid>
                       <Grid item container xs={12} md={8} direction="row" justifyContent="space-between">
                           <Grid item xs={12} md={4}>
-                              <Stack direction="column" spacing={3}>
+                              <Stack direction="column" spacing={3} sx={{padding:{xs:3,md:0}}}>
                                   {siteLinks.map((link) => (
                                       <CustomLink underline='none'>{link}</CustomLink>
                                   ))}
                               </Stack>
                           </Grid>
                           <Grid item xs={12} md={4}>
-                                <Stack direction="column" spacing={3}>
+                                <Stack direction="column" spacing={3} sx={{padding:{xs:3,md:0}}}>
                                   {otherLinks.map((link) => (
                                       <CustomLink underline='none'>{link}</CustomLink>
                                   ))}
                                 </Stack>
                           </Grid>
-                          <Grid item xs={12} md={4}>
+                          <Grid item xs={12} md={4} sx={{padding:{xs:3,md:0}}}>
                                 <Stack direction="column" spacing={3}>
                                   {helpLinks.map((link) => (
                                       <CustomLink underline='none'>{link}</CustomLink>
@@ -151,14 +150,68 @@ const Footer = () => {
                                 </Stack>
                           </Grid>
                       </Grid>
-                      <Grid item xs={12}>
+                      <Grid item xs={12} container sx={{justifyContent:{xs:"center", md:"flex-start"}}}>
                           <WhiteTypography>Copyright © Craftable</WhiteTypography>
                       </Grid>
                   </Grid>
               </Grid>
               <Grid item xs={0} md={1.25}></Grid>
           </Grid>
-        </Box></> : null}
+        </Box></> : 
+        <>
+            <Box bgcolor="primary.main" color="white" sx={{backgroundImage:`url(${backgroundImg})`, backgroundSize:"cover"}}>
+            <Grid container direction="row">
+              <Grid item xs={0} md={1.25}></Grid>
+              <Grid item container xs={12} md={9.5} direction="row" spacing={5} sx={{paddingY:5}}>
+                  <Grid item xs={12} container direction="row" spacing={5}>
+                      <Grid item xs={12} md={4}>
+                          <Stack direction="column" spacing={4}>
+                                  <Box sx={{display:"flex", justifyContent:{xs:"center", md:"flex-start"}}}>
+                                  <img src={require("../assets/footer_logo.png")}/>
+                                  </Box>
+                                  <Typography sx={{textAlign:{xs:"center", md:"left"}}} >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</Typography>
+                          </Stack>
+                      </Grid>
+                      <Grid item container xs={12} md={8} direction="row">
+                          <Grid item container xs={12} md={4} sx={{padding:{xs:3,md:0}}}>
+                              <Stack direction="column" spacing={3}>
+                                  {siteLinks.map((link) => (
+                                      <CustomLink underline='none'>{link}</CustomLink>
+                                  ))}
+                              </Stack>
+                          </Grid>
+                          <Grid item container xs={12} md={4} sx={{padding:{xs:3,md:0}}}>
+                                <Stack direction="column" spacing={3}>
+                                  {otherLinks.map((link) => (
+                                      <CustomLink underline='none'>{link}</CustomLink>
+                                  ))}
+                                </Stack>
+                          </Grid>
+                          <Grid item container xs={12} md={4} sx={{padding:{xs:3,md:0}}}>
+                                <Stack direction="column" spacing={3}>
+                                  {helpLinks.map((link) => (
+                                      <CustomLink underline='none'>{link}</CustomLink>
+                                  ))}
+                                </Stack>
+                          </Grid>
+                      </Grid>
+                      <Grid item container xs={12} md={6} alignItems="flex-end" sx={{justifyContent:{xs:"center", md:"flex-start"}}}>
+                          <WhiteTypography>Copyright © Craftable</WhiteTypography>
+                      </Grid>
+                      <Grid item container xs={12} md={6} sx={{justifyContent:{xs:"center", md:"flex-end"}}}>
+                        <Stack direction="row" spacing={3}>
+                            <FacebookIcon fontSize="large"/>
+                            <InstagramIcon fontSize="large"/>
+                            <TwitterIcon fontSize="large"/>
+                        </Stack>
+                    </Grid>
+                  </Grid>
+              </Grid>
+              <Grid item xs={0} md={1.25}></Grid>
+          </Grid>
+        </Box>
+        </>
+        }
       </footer>
   );
 };
